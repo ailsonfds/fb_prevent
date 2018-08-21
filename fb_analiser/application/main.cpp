2eae00bba2343e39755f3386da591ece9fba311e
Ailson F. dos Santos <ailsonfds94@gmail.com>
2018-08-20 22:54:46 -0300

    Undefined changes

--- fb_analiser/application/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[]){
-	std::cout << "Hello Motha F*ck!!" << std::endl;
-
-	return 0;
-}
--- fb_analiser/test/t_.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[]){
-	std::cout << "There's no such thing as \"test\"" << std::endl;
-
-	return 0;
-}
